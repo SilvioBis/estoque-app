4fcc914b39fef00f2800906d2a69a94b9700232a
Silvio bis <primoboby@hotmail.com>
2023-04-20 19:51:56 -0300

    Subindo Alterações

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,26 @@
 import { ClientesService } from './../services/clientes.service';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { Cliente } from '../models/Cliente.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule,CommonModule],
 })
 export class HomePage {
   constructor(private ClientesService: ClientesService) {
     this.buscarClientes();
   }
 
+  listaClientes: Cliente[] = [];
+
   buscarClientes(){
     this.ClientesService.getAll().subscribe(dados => {
-      console.log(dados)
+      this.listaClientes = dados  as Cliente[];
     });
   }
 }
